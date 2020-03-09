4ade0c8b251593224a79223033155e21ae484b39
Elijah wines <elijahwines84@yahoo.com>
2020-03-09 21:29:33 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,10 +6,14 @@ class Backer
     @name = name
     @backed_projects = []
   end
+  #takes a name on initilization accessible through attr_reader 
+  #initialized with backed_projects attr = to empty array
 
   def back_project(project_name)
     @backed_projects << project_name
     project_name.add_backer(self) unless project_name.backers.include?(self)
   end
+  #accepts project as an argument and stores it in a backed_projects array
+  #also adds the backer to the projects array
 
 end
